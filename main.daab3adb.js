876a8b937de953ed0c83966e6a96495103d7e881
Jose Gutierrez <jose.gutierrez@skillshare.com>
2022-03-02 11:06:28 -0500

    Updates

---
+++ main.daab3adb.js
@@ -0,0 +1,9 @@
+parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"amqR":[function(require,module,exports) {
+
+},{}],"j06p":[function(require,module,exports) {
+
+},{"./../img/naty-santy-hero.jpg":[["naty-santy-hero.77760bed.jpg","h9Lv"],"h9Lv"],"./../img/ampersand.svg":[["ampersand.a559330e.svg","hu8K"],"hu8K"],"./../img/bouquet-img.jpg":[["bouquet-img.4ab15c73.jpg","YVQ8"],"YVQ8"],"./../img/confirma-asistencia-bg.jpg":[["confirma-asistencia-bg.38e152a7.jpg","N6y0"],"N6y0"]}],"I7Ak":[function(require,module,exports) {
+
+},{"./style.css":"amqR","./frontend.css":"amqR","./css_002":"amqR","./post-2.css":"j06p","./wpforms-base.css":"amqR","./inline.css":"amqR"}],"d6sW":[function(require,module,exports) {
+"use strict";require("../css/main.css"),console.log("Naty y Santi");
+},{"../css/main.css":"I7Ak"}]},{},["d6sW"], null)
